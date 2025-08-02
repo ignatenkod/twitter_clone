5b654c0bf6135d4544e1e3c2728ed71c3fe401da
Diana <dvizh1203@gmail.com>
2025-08-02 14:51:42 +0300

    Create urls.py

---
+++ config/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('apps.posts.urls', namespace='posts')),
+    path('users/', include('apps.users.urls', namespace='users')),
+    path('notifications/', include('apps.notifications.urls', namespace='notifications')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
