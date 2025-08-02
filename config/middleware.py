47cd8140b7a6a1e5460dd14bd1a70e18ad1053ca
Diana <dvizh1203@gmail.com>
2025-08-02 14:59:32 +0300

    Create middleware.py

---
+++ config/middleware.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import AnonymousUser
+from channels.auth import AuthMiddlewareStack
+from channels.db import database_sync_to_async
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+
+User = get_user_model()
+
+class WebSocketJWTAuthMiddleware:
+    """Middleware для аутентификации WebSocket соединений через JWT"""
+    
+    def __init__(self, app):
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+        query_string = scope.get('query_string', b'').decode('utf-8')
+        query_params = dict(param.split('=') for param in query_string.split('&') if '=' in param)
+        
+        token = query_params.get('token', None)
+        
+        if token:
+            try:
+                access_token = AccessToken(token)
+                user = await self.get_user(access_token.payload.get('user_id'))
+                scope['user'] = user
+            except (InvalidToken, TokenError, User.DoesNotExist):
+                scope['user'] = AnonymousUser()
+        else:
+            scope['user'] = AnonymousUser()
+        
+        return await self.app(scope, receive, send)
+
+    @database_sync_to_async
+    def get_user(self, user_id):
+        return User.objects.get(id=user_id)
+
+
+def JWTAuthMiddlewareStack(app):
+    """Обертка для AuthMiddlewareStack с JWT аутентификацией"""
+    return WebSocketJWTAuthMiddleware(AuthMiddlewareStack(app))
