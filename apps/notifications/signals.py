5785f27480665a77e0234066dd902c5b08cc7a83
Diana <dvizh1203@gmail.com>
2025-08-02 14:57:11 +0300

    Update signals.py

--- apps/notifications/signals.py
+++ apps/notifications/signals.py
@@ -5,18 +5,30 @@ from django.utils.translation import gettext_lazy as _
 from apps.posts.models import Like, Comment
 from apps.users.models import Subscription
 from .models import Notification
+from apps.core.utils import send_notification_to_user
 
 
 @receiver(post_save, sender=Like)
 def create_like_notification(sender, instance, created, **kwargs):
     """Создание уведомления о новом лайке"""
     if created and instance.user != instance.post.author:
-        Notification.objects.create(
+        notification = Notification.objects.create(
             recipient=instance.post.author,
             sender=instance.user,
             notification_type=Notification.NotificationType.NEW_LIKE,
             message=_('{user} поставил лайк вашему посту').format(user=instance.user.username),
             post=instance.post
+        )
+        
+        # Отправка уведомления через WebSocket
+        send_notification_to_user(
+            instance.post.author.id,
+            {
+                'message': notification.message,
+                'type': str(notification.notification_type),
+                'created_at': notification.created_at.strftime('%Y-%m-%d %H:%M'),
+                'url': instance.post.get_absolute_url()
+            }
         )
 
 
@@ -24,12 +36,23 @@ def create_like_notification(sender, instance, created, **kwargs):
 def create_comment_notification(sender, instance, created, **kwargs):
     """Создание уведомления о новом комментарии"""
     if created and instance.author != instance.post.author:
-        Notification.objects.create(
+        notification = Notification.objects.create(
             recipient=instance.post.author,
             sender=instance.author,
             notification_type=Notification.NotificationType.NEW_COMMENT,
             message=_('{user} оставил комментарий к вашему посту').format(user=instance.author.username),
             post=instance.post
+        )
+        
+        # Отправка уведомления через WebSocket
+        send_notification_to_user(
+            instance.post.author.id,
+            {
+                'message': notification.message,
+                'type': str(notification.notification_type),
+                'created_at': notification.created_at.strftime('%Y-%m-%d %H:%M'),
+                'url': instance.post.get_absolute_url()
+            }
         )
 
 
@@ -37,9 +60,20 @@ def create_comment_notification(sender, instance, created, **kwargs):
 def create_subscription_notification(sender, instance, created, **kwargs):
     """Создание уведомления о новой подписке"""
     if created:
-        Notification.objects.create(
+        notification = Notification.objects.create(
             recipient=instance.subscribed_to,
             sender=instance.subscriber,
             notification_type=Notification.NotificationType.NEW_SUBSCRIBER,
             message=_('{user} подписался на вас').format(user=instance.subscriber.username)
+        )
+        
+        # Отправка уведомления через WebSocket
+        send_notification_to_user(
+            instance.subscribed_to.id,
+            {
+                'message': notification.message,
+                'type': str(notification.notification_type),
+                'created_at': notification.created_at.strftime('%Y-%m-%d %H:%M'),
+                'url': instance.subscriber.get_absolute_url()
+            }
         )
